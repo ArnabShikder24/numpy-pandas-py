0028518af8f944838808c057badc17a6820c9191
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-11-24 21:20:00 +0600

    numpy

---
+++ Numpy/main1.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+arr = np.array([1,2,3,4])
+# 2D Array:
+# arr = np.array([[1,2,3],[4,5,6]])
+
+# Special Arrays:
+# zeros = np.zeros((3,3)) #3x3 matrix of zeros
+# ones = np.ones((2,4)) #2x4 matrix of ones
+# random_arr = np.random.random((2,2))
+
+
+
+# Sequences:
+# arr = np.arange(0,10,2) # [0,2,,4,6,8]
+# arr_linspace = np.linspace(0, 1, 5)  # [0.  0.25  0.5  0.75  1.]
+
+# 3. Array Properties
+# print(arr.shape)       # Shape of the array
+# print(arr.size)        # Number of elements
+# print(arr.ndim)        # Number of dimensions
+# print(arr.dtype)       # Data type of elements
